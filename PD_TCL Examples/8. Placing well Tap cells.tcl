2c976d32d3d1b6cadccea9917321ef222a4de985
Ajaykumarburada <ajaykumarburada@gmail.com>
2026-02-24 17:14:37 +0530

    Update 8. Placing well Tap cells.tcl

--- PD_TCL Examples/8. Placing well Tap cells.tcl
+++ PD_TCL Examples/8. Placing well Tap cells.tcl
@@ -3,3 +3,4 @@ set tap_cell "tcbn28hpcplusbwp30p140hvt/TAPCELLBWP30P140"
 set spacing 30
 set patt "stagger"
 create_tap_cells -lib_cell $tap_cell -distance $spacing -pattern $patt -prefix TAP -skip_fixed_cell
+check_legality
